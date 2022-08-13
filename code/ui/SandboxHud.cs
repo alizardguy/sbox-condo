4c381cbab92ef23d32923ac1c63712277e19d1bf
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-08-13 16:56:45 -0700

    no more spawn menu for now

--- code/ui/SandboxHud.cs
+++ code/ui/SandboxHud.cs
@@ -19,7 +19,7 @@ public partial class SandboxHud : HudEntity<RootPanel>
 		RootPanel.AddChild<Health>();
 		RootPanel.AddChild<InventoryBar>();
 		RootPanel.AddChild<CurrentTool>();
-		RootPanel.AddChild<SpawnMenu>();
+		//RootPanel.AddChild<SpawnMenu>(); removes the main spawn menu
 		RootPanel.AddChild<Crosshair>();
 	}
 }
